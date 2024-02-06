83cc88b22e47fe8fbc36d01489f2656773bae9ae
Jason Everett <jaseeverett@gmail.com>
2024-02-06 10:26:44 +1000

    Updating comments

--- TestCode.R
+++ TestCode.R
@@ -26,12 +26,13 @@ PUs <- splnr_get_planningUnits(Bndry, landmass, PU_size, Shape)
 
 gfw_data <- get_gfwData('Australia', "2022-01-01", "2022-12-31", "yearly", cCRS = cCRS, compress = TRUE)
 
-# Try sf::st_interpolate_aw. JDE - Not sure if this is the best option....
+# Try sf::st_interpolate_aw. JDE - Not sure if this is the best option but it works.
 gfw_PU <- sf::st_interpolate_aw(gfw_data, PUs, extensive = TRUE)
 
-PUs$Apparent.Fishing.Hours <- 0
-PUs$Apparent.Fishing.Hours[as.numeric(rownames(gfw_PU))] <- gfw_PU$Apparent.Fishing.Hours
+PUs$Apparent.Fishing.Hours <- 0 # Add column to PUs
+PUs$Apparent.Fishing.Hours[as.numeric(rownames(gfw_PU))] <- gfw_PU$Apparent.Fishing.Hours # Put corresponding data in PUs
 
+# Lets plot the interpolated data next to the original data.
 gg1 <- ggplot() +
   geom_sf(data = PUs, aes(fill = log10(Apparent.Fishing.Hours)))
 
@@ -39,8 +40,10 @@ gg2 <- ggplot() +
   geom_sf(data = gfw_data %>% sf::st_crop(PUs), aes(fill = log10(`Apparent Fishing Hours`),
                                                     colour = log10(`Apparent Fishing Hours`)))
 
-patchwork::wrap_plots(gg1, gg2, ncol = 1)
+patchwork::wrap_plots(gg1, gg2, ncol = 1) # Looks good
 
+
+## This still doesn't work. We will update data_to_planning_grid in due course.
 gfw_data_gridded <- data_to_planning_grid(planning_grid = PUs, dat = gfw_data, name = "FishHrs",
                                           meth = "average",
                                           sf_col_layer_names = "Apparent Fishing Hours")
